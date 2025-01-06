490e34c84d89dfe3bf9ed2c1667ef6ce13e43f7c
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-01-06 11:47:30 +0100

    Init project

---
+++ src/main/java/com/coresaken/memApp/MemAppApplication.java
@@ -0,0 +1,13 @@
+package com.coresaken.memApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coresaken/memApp/MemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coresaken.memApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
